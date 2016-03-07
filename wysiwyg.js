95c4ae9acfb13085f031645f351dceb3bf57f4c5
Tim Maddux <timmaddux@tims-mbp.attlocal.net>
2016-03-06 20:48:02 -0600

    Commented instructions in js file.

--- wysiwyg.js
+++ wysiwyg.js
@@ -1,12 +1,101 @@
+// 1. Create an array of objects that represents famous people (see structure below).
+var famousPeople = [
+{
+  title: "Samurai",
+  name: "Tomoe Gozen",
+  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+  lifespan: {
+    birth: 1747,
+    death: 1797
+  }
+},
+{
+  title: "Samurai",
+  name: "Tomoe Gozen",
+  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+  lifespan: {
+    birth: 1747,
+    death: 1797
+  }
+},
+{
+  title: "Samurai",
+  name: "Tomoe Gozen",
+  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+  lifespan: {
+    birth: 1747,
+    death: 1797
+  }
+},
+{
+  title: "Samurai",
+  name: "Tomoe Gozen",
+  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+  lifespan: {
+    birth: 1747,
+    death: 1797
+  }
+},
+{
+  title: "Samurai",
+  name: "Tomoe Gozen",
+  bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+  image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+  lifespan: {
+    birth: 1747,
+    death: 1797
+  }
+},
+];
+
+// 2. Create a text input in your DOM.
+document.getElementById("textInputDiv").innerHTML += "<input type='text' id='textInput' value='Edit their bio!'>";
+
+// 3. Beneath that, create a container, block element in your DOM.
+document.getElementById("textInputDiv").firstChild.innerHTML += "<article id='container'></article>";
+// I THINK I NEED TO ACTUALLY CREATE AN ARTICLE WITH THE STUFF I NEED TO LEARN FROM 
+// MYERS CHAPTERS 64 - 68. 
 
 
-// {
-//   title: "Samurai",
-//   name: "Tomoe Gozen",
-//   bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-//   image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg"
-//   lifespan: {
-//     birth: 1747,
-//     death: 1797
-//   }
-// };
+// 4. Create a DOM element for each of the objects inside the container. Style your person 
+// elements however you like.
+
+
+// 5. For every even numbered element, have a light yellow background.
+
+
+// 6. For every odd numbered element, have a light blue background.
+
+
+// 7. Each element's DOM structure should be as shown below.
+
+
+// 8. When you click on one of the person elements, a dotted border should appear around it.
+
+
+// 9. When you click on one of the person elements, the text input should immediately 
+// gain focus so that you can start typing.
+
+
+// 10. When there is a highlighted person element, and you begin typing in the input box, 
+// the person's biography should be immediately bound to what you are typing, letter by letter.
+
+
+// 11. When you press the enter/return key when typing in the input field, then the content of 
+// the input field should immediately be blank.
+
+
+
+
+
+
+
+
+
+
+
+
